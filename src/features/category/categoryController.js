4d7f428f68fd922c2366bef9b02beacbce521b8d
TAMZID <tamzid2233@gmail.com>
2026-01-27 15:37:44 +0600

    add "get and update"category functionality

--- src/features/category/categoryController.js
+++ src/features/category/categoryController.js
@@ -25,3 +25,34 @@ export const allCategory = asyncHandler(async (req, res, next) => {
         categories
     });
 });
+
+//Get single category 
+export const singleCategory = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    if (!id) return next(new ErrorHandler("Please provide a category id", 400));
+    const category = await Category.findById(id);
+    if (!category) return next(new ErrorHandler("Category not found", 404));
+    res.status(200).json({
+        success: true,
+        message: "Category fetched successfully",
+        category
+    });
+});
+
+//update category 
+export const updateCategory = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    if (!name) return next(new ErrorHandler("Please provide a new category name", 400));
+    if (!id) return next(new ErrorHandler("Please provide a category id", 400));
+    //is exists
+    const isExists = await Category.findOne({ name });
+    if (isExists) return next(new ErrorHandler("Category already exists", 400));
+    const category = await Category.findByIdAndUpdate(id, { name }, { new: true, runValidators: true });
+    if (!category) return next(new ErrorHandler("Category not found", 404));
+    res.status(200).json({
+        success: true,
+        message: "Category updated successfully",
+        category
+    });
+});
--- src/features/category/categoryRoute.js
+++ src/features/category/categoryRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createCategory,allCategory } from "./categoryController.js";
+import { createCategory,allCategory,singleCategory,updateCategory } from "./categoryController.js";
 import adminOnly from "../../middleware/admin.js";
 import isAuthenticated from "../../middleware/authMiddleware.js";
 
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.post("/", isAuthenticated, adminOnly, createCategory);
 router.get("/", isAuthenticated, adminOnly, allCategory);
+router.get("/:id", isAuthenticated, adminOnly, singleCategory);
+router.put("/:id", isAuthenticated, adminOnly, updateCategory);
 
 export default router;
